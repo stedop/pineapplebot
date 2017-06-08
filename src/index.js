bb30bd7d8fe19e57a766ee3f2dd665cc9897af65
ste <dopstephne@gmail.com>
2017-06-08 16:27:37 +0100

    Updated the config to include log files and updated the example config to match the reality of the code now.
    Added the logger to the index and passing it to the bot.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 'use strict';
 
 import Config from 'config';
+import winston from 'winston';
 import PineappleBot from './PineappleBot';
 
 try {
-    const pineAppleBot = new PineappleBot(Config);
+    const narc = new winston.Logger(
+        {
+            level: (Config.get('Debug') === true) ? 'debug' : 'info',
+            transports: [
+                new (winston.transports.File)({ filename: Config.get('Log.file') })
+            ]
+        }
+    );
+    const pineAppleBot = new PineappleBot(Config, narc);
     pineAppleBot.go();
 } catch ( error ) {
     console.log('error', error.stack);
